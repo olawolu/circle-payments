9f074efcb783a8757aa8d1bbb9a014b35833645b
Gbenga <omg.tosine@gmail.com>
2021-04-30 18:34:06 +0100

    implemented openpgp for encrypting sensitive data

---
+++ pkg/payments/card.go
@@ -0,0 +1,39 @@
+package payments
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/armor"
+)
+
+func Encrypt(data CardDetails, key string) string {
+	passphrase := []byte(key)
+
+	message, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buffer := bytes.NewBuffer(nil)
+	w, err := armor.Encode(buffer, openpgp.PublicKeyType, make(map[string]string))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plaintext, err := openpgp.SymmetricallyEncrypt(w, passphrase, nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = plaintext.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plaintext.Close()
+	w.Close()
+	return buffer.String()
+}
